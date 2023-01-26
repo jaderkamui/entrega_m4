c47c12b6c85b62afe5d3edb8c7093b7eb6c3ad2d
Jaderkamui <tecnico.jader@gmail.com>
2023-01-25 21:28:55 -0300

    bodega

---
+++ assets/js/Producto.js
@@ -0,0 +1,51 @@
+let productosStorage = JSON.parse(localStorage.getItem("productos")) || [];
+
+export default class Producto{
+    constructor(id, nombre = "", descripcion = "Sin descripción", precio = 999999, stock = 0){
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion= descripcion
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    getProducts(){
+        productosStorage = productosStorage = JSON.parse(localStorage.getItem("productos")) || []
+        return productosStorage;
+    }
+    getProduct(){
+        productosStorage = JSON.parse(localStorage.getItem("productos")) || []
+        return productosStorage.find(producto => producto.id == this.id);
+    }
+    deleteProduct(){
+        productosStorage = JSON.parse(localStorage.getItem("productos")) || []
+        productosStorage = productosStorage.filter(producto => producto.id != this.id)
+        localStorage.setItem("productos", JSON.stringify(productosStorage))
+        return productosStorage;
+    }
+    updateProduct(){
+        productosStorage = JSON.parse(localStorage.getItem("productos")) || []
+        let producto = productosStorage.find(producto => producto.id ==this.id)
+        producto.nombre= this.nombre;
+        producto.descripcion= this.descripcion;
+        producto.precio = this.precio;
+        producto.stock = this.stock;
+        localStorage.setItem("productos", JSON.stringify(productosStorage))
+        return producto;
+    }
+    addProduct(){
+        productosStorage = JSON.parse(localStorage.getItem("productos")) || []
+        productosStorage.push(
+            {
+                id: this.id,
+                nombre: this.nombre,
+                descripcion: this.descripcion,
+                precio: this.precio, 
+                stock: this.stock
+            }
+            )
+            localStorage.setItem("productos", JSON.stringify(productosStorage))
+        return productosStorage
+    }
+
+}
